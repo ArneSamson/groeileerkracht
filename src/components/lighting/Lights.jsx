2ec945f1cb6fbc2ab0c227dd749ae628dae17053
ArneSamson <arnesamson007@gmail.com>
2026-05-03 12:00:56 +0200

    fix: bigger shadowbox

--- src/components/lighting/Lights.jsx
+++ src/components/lighting/Lights.jsx
@@ -2,6 +2,8 @@ import { Shadow, SoftShadows } from "@react-three/drei";
 import React from "react";
 
 export default function Lights() {
+
+    const mapSizeMultiplyer = 4;
     return (
         <>
             <ambientLight intensity={2} />
@@ -9,13 +11,13 @@ export default function Lights() {
                 castShadow
                 position={[2, 2, 2]}
                 intensity={1}
-                shadow-mapSize={[2048, 2048]}
+                shadow-mapSize={[1024 * mapSizeMultiplyer, 1024 * mapSizeMultiplyer]}
                 shadow-camera-near={0.1}
                 shadow-camera-far={100}
-                shadow-camera-top={50}
-                shadow-camera-right={50}
-                shadow-camera-bottom={-50}
-                shadow-camera-left={-50}
+                shadow-camera-top={70}
+                shadow-camera-right={70}
+                shadow-camera-bottom={-70}
+                shadow-camera-left={-70}
                 shadow-bias={0.0}
                 shadow-normalBias={0}
             />
